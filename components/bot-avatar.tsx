4b093f86181436bea8eb18ffab81ba43da3dcd26
Shawn Kebel <fxrender@outlook.com>
2023-12-07 22:00:26 -0600

    :children_crossing: Add new `components`: BotAvatar, Empty, Loader, and UserAvatar

---
+++ components/bot-avatar.tsx
@@ -0,0 +1,9 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+export const BotAvatar = () => {
+  return (
+    <Avatar className="h-12 w-12">
+      <AvatarImage className="p-1" src="/Einstein-bot.png" />
+    </Avatar>
+  );
+};
